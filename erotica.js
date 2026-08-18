e5f81396c226e6c66d8ba3837b786aefb4c8e04d
Wal'demar <bob07mail@gmail.com>
2026-08-18 11:00:07 +0300

    Update erotica.js

--- erotica.js
+++ erotica.js
@@ -14,17 +14,16 @@
                         '<path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" fill="currentColor"/>' +
                     '</svg>' +
                 '</div>' +
-                '<div class="menu__text">Фільми 18+ / Еротика</div>' +
+                '<div class="menu__text">Фільми 18+</div>' +
             '</li>');
 
             button.on('hover:enter', function () {
                 Lampa.Activity.push({
-                    title: 'Повнометражні фільми 18+',
+                    title: 'Фільми 18+',
                     component: 'category_full',
                     source: 'tmdb',
-                    include_adult: true,
-                    certification_country: 'US',
-                    'certification.gte': 'NC-17',
+                    genres: '10749',
+                    include_adult: 'true',
                     card_type: 'movie',
                     page: 1
                 });
